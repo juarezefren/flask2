cf1ca5742a801575a5c4464e5f6c1b03a9aa2b04
Efrén Juárez-Castillo <46583863+juarezefren@users.noreply.github.com>
2024-06-05 17:51:33 -0600

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+import logging
 
 app = Flask(__name__)
+
+# Configurar el registro
+logging.basicConfig(level=logging.DEBUG)
 
 # Cargar el modelo entrenado
 model = joblib.load('model.pkl')
@@ -11,15 +15,22 @@ model = joblib.load('model.pkl')
 def predict():
     # Obtener los datos enviados en el request
     data = request.get_json()
+    app.logger.debug(f'Datos recibidos: {data}')
     
     # Convertir los datos a un DataFrame con nombres de columnas adecuados
-    data_df = pd.DataFrame([data], columns=['abdomen', 'antena'])
-    
-    # Realizar predicciones
-    predictions = model.predict(data_df)
-    
-    # Devolver las predicciones como respuesta JSON
-    return jsonify({'categoria': predictions[0]})
+    try:
+        data_df = pd.DataFrame([data], columns=['abdomen', 'antena'])
+        app.logger.debug(f'DataFrame creado: {data_df}')
+        
+        # Realizar predicciones
+        predictions = model.predict(data_df)
+        app.logger.debug(f'Predicciones: {predictions}')
+        
+        # Devolver las predicciones como respuesta JSON
+        return jsonify({'categoria': predictions[0]})
+    except Exception as e:
+        app.logger.error(f'Error en la predicción: {str(e)}')
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
